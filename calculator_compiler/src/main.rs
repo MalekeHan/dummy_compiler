df466128d5f6236bd13653fae7b860b636ac5215
MalekeHan <malekehanson@gmail.com>
2024-02-23 01:09:30 -0800

    testing construction

--- calculator_compiler/src/main.rs
+++ calculator_compiler/src/main.rs
@@ -2,23 +2,45 @@ use ast::*;
 use calculator_compiler::*;
 
 fn main() {
-    // you need to declare variable names as rust variables
-    let x = "foo";
-    let y = "bar";
+    // // you need to declare variable names as rust variables
+    // let x = "foo";
+    // let y = "bar";
 
-    let s = stmt! {
-        // parentheses around statements, and parentheses in guards are mandatory
-        (while (5) {
-        (if (3) {
-            (x = 5);
-            (if (5) {
-                (y = ((x + 3) - 5))
+    // let s = stmt! {
+    //     // parentheses around statements, and parentheses in guards are mandatory
+    //     (while (5) {
+    //     (if (3) {
+    //         (x = 5);
+    //         (if (5) {
+    //             (y = ((x + 3) - 5))
+    //         } else {
+    //         })
+    //     } else {
+    //     })
+    //     })
+    // };
+
+    // println!("Built: {:#?}", s);
+    
+    let x = "x";
+    let y = "y";
+    let z = "z";
+
+    let complex_logic = stmt! {
+        (while (x < 10) {
+            (if (x > 5) {
+                (y = (y + 1));
             } else {
-            })
-        } else {
-        })
+                (z = (z - 1));
+            });
+            (x = (x + 1));
         })
     };
 
-    println!("Built: {:#?}", s);
+    println!("Complex Logic: {:#?}", complex_logic);
+
+
+
+
+
 }
