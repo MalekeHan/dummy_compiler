80e4e1d33baa1f96bc83fac4f7a4a67d35458380
MalekeHan <malekehanson@gmail.com>
2024-02-23 01:21:33 -0800

    okay

--- calculator_compiler/src/main.rs
+++ calculator_compiler/src/main.rs
@@ -1,46 +1,43 @@
 use ast::*;
 use calculator_compiler::*;
 
-fn main() {
-    // you need to declare variable names as rust variables
-    let x = "foo";
-    let y = "bar";
+// fn main() {
+//     // you need to declare variable names as rust variables
+//     let x = "foo";
+//     let y = "bar";
 
-    let s = stmt! {
-        // parentheses around statements, and parentheses in guards are mandatory
-        (while (5) {
-        (if (3) {
-            (x = 5);
-            (if (5) {
-                (y = ((x + 3) - 5))
-            } else {
-            })
-        } else {
-        })
+//     let s = stmt! {
+//         // parentheses around statements, and parentheses in guards are mandatory
+//         (while (5) {
+//         (if (3) {
+//             (x = 5);
+//             (if (5) {
+//                 (y = ((x + 3) - 5))
+//             } else {
+//             })
+//         } else {
+//         })
+//         })
+//     };
+
+//     println!("Built: {:#?}", s);
+
+
+// }
+fn main() {
+    let a = "a";
+    let b = "b";
+    let counter = "counter";
+
+    let initialization_and_loop = stmt! {
+        (a = 10);
+        (b = 20);
+        (while (counter < 100) {
+            (a = (a + b));
+            (b = (b + 1));
+            (counter = (counter + 1));
         })
     };
 
-    println!("Built: {:#?}", s);
-    
-    // let x = "x";
-    // let y = "y";
-    // let z = "z";
-
-    // let complex_logic = stmt! {
-    //     (while (x < 10) {
-    //         (if (x > 5) {
-    //             (y = (y + 1));
-    //         } else {
-    //             (z = (z - 1));
-    //         });
-    //         (x = (x + 1));
-    //     })
-    // };
-
-    // println!("Complex Logic: {:#?}", complex_logic);
-
-
-
-
-
+    println!("Initialization and Loop: {:#?}", initialization_and_loop);
 }
