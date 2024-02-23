ffc8edb80e117429df99851771d812754c3a2f07
MalekeHan <malekehanson@gmail.com>
2024-02-23 01:35:29 -0800

    okay

--- calculator_compiler/src/main.rs
+++ calculator_compiler/src/main.rs
@@ -3,20 +3,47 @@ use calculator_compiler::*;
 
 fn main() {
     // Declare variable names as Rust variables
-    let x = "foo";
-    let y = "bar";
-    let z = "baz";
+    let x = "x";
+    let y = "y";
+    let z = "z";
 
+    // Original statement
     let s = stmt! {
-        (while (x < 10) {
-            (if ((x + 2) <= 10) {
-                (x = (x + 1)); // Increment x
+        (while (5) {
+            (if (3) {
+                (x = 5);
+                (if (5) {
+                    (y = ((x + 3) - 5))
+                } else {
+                })
             } else {
-                // Ensure a no-op or valid statement here if your macro doesn't support empty blocks
-            };)
-            (z = ((x * 2) / (y + 3)));
-        };)
+            })
+        })
     };
 
-    println!("While with nested if-else: {:#?}", s);
+    // New complex statement
+    let new_statement = stmt! {
+        (if ((x * 2) > 10) {
+            (y = (x + 1));
+            (z = (y * 2));
+            (if ((z - y) < 5) {
+                (x = (z / 2))
+            } else {
+                (x = 0)
+            })
+        } else {
+            (while ((x + y) < 20) {
+                (x = (x + 1));
+                (y = (y + 1));
+                (if ((x % 2) == 0) {
+                    (z = (x * 3))
+                } else {
+                    (z = (y / 2))
+                })
+            })
+        })
+    };
+
+    println!("Original: {:#?}", s);
+    println!("New Statement: {:#?}", new_statement);
 }
