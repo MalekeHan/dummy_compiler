26553a0ea4e91f2038310d31933350d8e573d8c7
MalekeHan <malekehanson@gmail.com>
2024-02-27 00:24:15 -0800

    added build.rs

--- calculator_compiler/build.rs
+++ calculator_compiler/build.rs
@@ -1,7 +1,10 @@
 fn main() {
-    // Link against libstdc++
+    //  link with libstdc++
     println!("cargo:rustc-link-lib=stdc++");
 
-    // Specify the path to libstdc++
+    // specify the search path for libstdc++.so
     println!("cargo:rustc-link-search=native=/usr/lib64");
+
+    // build script is re-executed only if it changes
+    println!("cargo:rerun-if-changed=build.rs");
 }
